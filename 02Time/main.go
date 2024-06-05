6f7672c618837d2bcbc07755a33299f65cc54774
Harsh panwar <panwarharshwardhan@email.com>
2024-06-05 10:54:19 +0530

    Time

---
+++ 02Time/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("welcome to golang course toke abote Time package")
+	prsantTIme := time.Now()
+	fmt.Println(prsantTIme.Format("2006/01/02"))
+	fmt.Printf("type %T", prsantTIme)
+}
