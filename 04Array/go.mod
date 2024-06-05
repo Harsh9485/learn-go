3fbc93820b18f1221fc5d92d75951d925deb044f
Harsh panwar <panwarharshwardhan@email.com>
2024-06-05 17:06:10 +0530

    learn Array

---
+++ 04Array/go.mod
@@ -0,0 +1,3 @@
+module GoArray
+
+go 1.22.3
---
+++ 04Array/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var fruitList [4]string
+	// fruitList[0] = "apple"
+	// fruitList[1] = "banana"
+	// fruitList[3] = "orange"		 0     1	2	3	
+	// fmt.Println(fruitList) --> [apple banana  orange]
+
+	
+	// var vegList [4]string = [4]string{"potato", "tomato", "onion", "carrot"} 
+}
