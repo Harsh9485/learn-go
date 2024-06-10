3a2c428e805624c9207f77ba2d8fb1f7e66c9bd3
Harsh panwar <panwarharshwardhan@email.com>
2024-06-10 12:15:37 +0530

    fix

--- 14WebRequest/main.go
+++ 14WebRequest/main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"fmt"
-	"math"
-	// "net/http"
+
+	"net/http"
 )
 
 func main() {
-	// URL := "https://google.com"
-	// fmt.Println("welcome to golang course talk abote Web Request ")
-	// res, err := http.Get(URL)
-	// errChack(err)
-	// defer res.Body.Close() // close the body when we are done with it
+	URL := "https://google.com"
+	fmt.Println("welcome to golang course talk abote Web Request ")
+	res, err := http.Get(URL)
+	errChack(err)
+	defer res.Body.Close() // close the body when we are done with it
 	
-	// 2 ** 4
+	
 	
 }
 
