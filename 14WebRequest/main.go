e2b8e0e2461f4b47af0cf53049da043d6a004277
Harsh panwar <panwarharshwardhan@email.com>
2024-06-11 23:24:08 +0530

    leran how to send Get request

--- 14WebRequest/main.go
+++ 14WebRequest/main.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io"
+	
 
 	"net/http"
 )
 
 func main() {
-	URL := "https://google.com"
+	URL := "https://github.com/" // --> any url is works
 	fmt.Println("welcome to golang course talk abote Web Request ")
-	res, err := http.Get(URL)
+	res, err := http.Get(URL) // --> send get request to the url
 	errChack(err)
 	defer res.Body.Close() // close the body when we are done with it
-	
-	
+	contant, _ := io.ReadAll(res.Body) // read the data from the body and convert []byte 
+	fmt.Print(string(contant)) // convert the string and print it
 	
 }
 
