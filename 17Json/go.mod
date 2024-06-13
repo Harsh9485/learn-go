041dd84b86e8c1ee5e018cd998538388540096f2
Harsh panwar <panwarharshwardhan@email.com>
2024-06-13 17:20:21 +0530

    JSON

---
+++ 17Json/go.mod
@@ -0,0 +1,3 @@
+module GOJson
+
+go 1.22.3
---
+++ 17Json/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func main() {
+	// URL := "http://localhost:3000" // --> any url is works
+	fmt.Println("welcome to golang course talk abote send from data ")
+	lenCode := []Course{
+		{
+			Name: "Golang",
+			Price: 100,
+			Duration: 2,
+			Rating: 5.0,
+			Pasword: "cdad41d5sa",
+			Flag: []string{"wev-dev", "Backend"},
+		},
+		{
+			Name: "Node js",
+			Price: 200,
+			Duration: 1,
+			Rating: 4.5,
+			Pasword: "dfdf4df1d",
+			Flag: nil,
+		},
+	}
+	jsonFormat, err := json.MarshalIndent(lenCode, "", "\t")
+	errChack(err)
+	fmt.Printf("%s\n",jsonFormat)
+
+}
+
+func errChack(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+type Course struct {
+	Name string `json:"coursName"`
+	Price int 
+	Duration int 
+	Rating float32
+	Pasword string `json:"-"`
+	Flag []string  `json:"tage,omitempty"`
+}
