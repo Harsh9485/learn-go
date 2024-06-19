e781243e1730d5405018aa9039d081be9a228f4d
Harsh panwar <panwarharshwardhan@email.com>
2024-06-19 11:46:42 +0530

    learn more about webRequest

--- 14WebRequest/main.go
+++ 14WebRequest/main.go
@@ -1,23 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
-	
-
+	"io/ioutil"
 	"net/http"
 )
 
 func main() {
-	URL := "https://github.com/" // --> any url is works
+	URL := "https://jsonplaceholder.typicode.com/todos/1" // --> any url is works
 	fmt.Println("welcome to golang course talk abote Web Request ")
 	res, err := http.Get(URL) // --> send get request to the url
 	errChack(err)
 	defer res.Body.Close() // close the body when we are done with it
-	contant, _ := io.ReadAll(res.Body) // read the data from the body and convert []byte 
-	fmt.Print(string(contant)) // convert the string and print it
+	contant, _ := ioutil.ReadAll(res.Body) // read the data from the body and convert []byte 
+	var data interface{}
+	json.Unmarshal(contant, &data) // convert
+	fmt.Print(data) // convert the string and print it
 	
 }
+
+type data struct {}
 
 func errChack(err error) {
 	if err!= nil {
